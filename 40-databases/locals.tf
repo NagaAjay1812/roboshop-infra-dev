f1c2ef7c7e3f67c483d143874c9185dc76743851
Naga Ajay <nagaajay@Nagas-Mac-mini.local>
2026-03-09 15:50:01 -0400

    “actions”

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -1,19 +1,19 @@
 locals {
   ami_id        = var.ami_id
-  bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
 
   common_tags = {
     Name        = var.project
     Environment = var.environment
     Terraform   = "true"
   }
-  bastion_final_tags = merge(
+  mongodb_final_tags = merge(
     local.common_tags,
     {
-      Name = "${var.project}-${var.environment}-bastion"
+      Name = "${var.project}-${var.environment}-mongodb"
     },
-    var.bastion_tags
+    var.mongodb_tags
   )
   #public subnet in 1a availibility zone
-  public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
+  database_subnet_ids = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
 }
