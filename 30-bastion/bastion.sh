dd0f3e80cb99a50877b0f5836296f32089407210
Naga Ajay <nagaajay@Nagas-Mac-mini.local>
2026-03-10 12:48:36 -0400

    “actions”

--- 30-bastion/bastion.sh
+++ 30-bastion/bastion.sh
@@ -13,7 +13,7 @@ lvextend -r -L+30G /dev/mapper/RootVG-homeVol
 # This makes the extra 30GB available inside the /home directory.
 xfs_growfs /home
 
-#Install terffaorm
+#Install terraform
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
