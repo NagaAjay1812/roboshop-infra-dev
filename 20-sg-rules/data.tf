b62e0b0cd06de9a27d5a11787f9119ba0bdf2a21
Naga Ajay <nagaajay@Nagas-Mac-mini.local>
2026-03-10 15:08:16 -0400

    “actions”

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -1,7 +1,6 @@
 data "aws_ssm_parameter" "bastion_sg_id" {
   name = "/${var.project}/${var.environment}/bastion_sg_id"
 }
-
 data "aws_ssm_parameter" "mongodb_sg_id" {
   name = "/${var.project}/${var.environment}/mongodb_sg_id"
 }
@@ -11,4 +10,9 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
 data "aws_ssm_parameter" "user_sg_id" {
   name = "/${var.project}/${var.environment}/user_sg_id"
 }
-
+data "aws_ssm_parameter" "redis_sg_id" {
+  name = "/${var.project}/${var.environment}/user_sg_id"
+}
+data "aws_ssm_parameter" "cart_sg_id" {
+  name = "/${var.project}/${var.environment}/cart_sg_id"
+}
--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -40,3 +40,37 @@ resource "aws_security_group_rule" "mongodb_user" {
   # target traffic
   security_group_id = local.mongodb_sg_id
 }
+
+
+resource "aws_security_group_rule" "bastion_redis" {
+  type      = "ingress"
+  from_port = 22
+  to_port   = 22
+  protocol  = "tcp"
+  # where traffic come from(source)
+  source_security_group_id = local.bastion_sg_id
+  # target traffic
+  security_group_id = local.redis_sg_id
+}
+
+resource "aws_security_group_rule" "redis_user" {
+  type      = "ingress"
+  from_port = 6379
+  to_port   = 6379
+  protocol  = "tcp"
+  # where traffic come from(source)
+  source_security_group_id = local.user_sg_id
+  # target traffic
+  security_group_id = local.redis_sg_id
+}
+
+resource "aws_security_group_rule" "redis_cart" {
+  type      = "ingress"
+  from_port = 6379
+  to_port   = 6379
+  protocol  = "tcp"
+  # where traffic come from(source)
+  source_security_group_id = local.cart_sg_id
+  # target traffic
+  security_group_id = local.redis_sg_id
+}
