1f906d8e8ed3717fe4063b6f8d8b73558600031d
Naga Ajay <nagaajay@Nagas-Mac-mini.local>
2026-03-13 00:49:57 -0400

    “actions”

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -115,5 +115,42 @@ resource "terraform_data" "bootstrap_mysql" {
   }
 }
 
+resource "aws_instance" "mysql" {
+  ami           = local.ami_id # we paramatrized
+  instance_type = var.instance_type
+  subnet_id     = local.database_subnet_ids
 
+  # Reference the security group ID here
+  vpc_security_group_ids = [local.mysql_sg_id]
+
+  # Optional: Add tags to the instance for identification
+  tags = local.redis_final_tags
+}
+
+resource "terraform_data" "bootstrap_rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id
+
+  ]
+  connection {
+    type     = "ssh"
+    user     = "ec2-user" # or 'ec2-user', etc.
+    password = "DevOps321"
+    host     = aws_instance.rabbitmq.private_ip
+    timeout  = "5m"
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh" # we are copying the script in mongodb server
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sleep 60",
+      "chmod +x /tmp/bootstrap.sh",        # giving execute access to that script
+      "sudo sh /tmp/bootstrap.sh rabbitmq" # now we are executing the script and we are passing redis component
+    ]
+  }
+}
 
