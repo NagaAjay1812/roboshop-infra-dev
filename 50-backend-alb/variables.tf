05e9128885a37b8bfd3d99bbc64da8794f15aba2
Naga Ajay Ragyari <a918106209213@nagas-macbook.mynetworksettings.com>
2026-03-13 14:04:26 -0400

    “actions”

--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -6,26 +6,11 @@ variable "environment" {
   default = "dev"
 }
 
-variable "ami_id" {
-  default = "ami-0220d79f3f480ecf5"
+
+variable "backend_tags" {
+  default = {}
 }
 
-variable "instance_type" {
-  default = "t3.micro"
-}
-
-variable "mongodb_tags" {
-  default = {}
-}
-variable "redis_tags" {
-  default = {}
-}
-variable "mysql_tags" {
-  default = {}
-}
-variable "rabbitmq_tags" {
-  default = {}
-}
 variable "zone_id" {
   default = "Z07326442Z8C3IRLJ3030"
 }
