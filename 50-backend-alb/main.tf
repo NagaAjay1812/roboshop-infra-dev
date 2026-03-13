8a27ca3cd89275f7fc8a636327b8c3ed9a356688
Naga Ajay Ragyari <a918106209213@nagas-macbook.mynetworksettings.com>
2026-03-13 14:48:05 -0400

    “actions”

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -1,12 +1,41 @@
-resource "aws_lb" "main" {
+resource "aws_lb" "backend_alb" {
   name               = "backend-alb-${var.project}-${var.environment}"
-  internal           = false
+  internal           = true
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.backend_alb_sg_id.id]
+  security_groups    = [local.backend_alb_sg_id.id]
   subnets            = local.private_subnet_id
 
   enable_deletion_protection = false
 
 
   tags = local.backend_alb_final_tags
+}
+
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.backend_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "<h>Hi, Im from http backend ALB</h1>"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_route53_record" "backend_alb" {
+  zone_id = var.zone_id
+  name    = "*.backend-alb-${var.environment}-${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_elb.backend_alb.dns_name
+    zone_id                = aws_elb.backend_alb.zone_id
+    evaluate_target_health = true
+  }
 }
