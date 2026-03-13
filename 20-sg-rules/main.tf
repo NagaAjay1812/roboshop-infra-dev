5fd83fe30aa63fa311393917db6f543d2aa8decd
Naga Ajay <nagaajay@Nagas-Mac-mini.local>
2026-03-13 00:28:44 -0400

    “actions”

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -88,3 +88,15 @@ resource "aws_security_group_rule" "bastion_mysql" {
   # target traffic
   security_group_id = local.mysql_sg_id
 }
+
+# baxkend-alb
+resource "aws_security_group_rule" "bastion_backend_alb" {
+  type      = "ingress"
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+  # where traffic come from(source)
+  source_security_group_id = local.bastion_sg_id
+  # target traffic
+  security_group_id = local.backend_alb_sg_id
+}
