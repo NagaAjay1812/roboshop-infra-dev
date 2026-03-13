dcde4a54486e3b9400b9caba100ed17fdce20fa5
Naga Ajay Ragyari <a918106209213@nagas-macbook.mynetworksettings.com>
2026-03-13 15:45:48 -0400

    “actions”

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,6 +1,6 @@
 module "sg" {
   count       = length(var.sg_names)
-  source      = "../../terraform-aws-sg" # here we are refering from local noy from git -> i will refer from git give like this git::https?ref=main
+  source      = "git::https://github.com/NagaAjay1812/terraform-aws-sg.git?ref=main" # here we are refering from local noy from git -> i will refer from git give like this git::https?ref=main
   project     = var.project
   environment = var.environment
   sg_name     = var.sg_names[count.index]
